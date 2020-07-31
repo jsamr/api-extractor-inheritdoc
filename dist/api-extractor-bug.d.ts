04579e959430221c49006eb805ec24b899374c4a
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-07-31 09:50:53 -0300

    Initial Commit

---
+++ dist/api-extractor-bug.d.ts
@@ -0,0 +1,20 @@
+
+/**
+ * @public
+ */
+export declare interface A {
+  /**
+   * This is a description of foo, which usually bars.
+   */
+  foo: string;
+}
+
+/**
+ * @public
+ */
+export declare interface B {
+  /** {@inheritDoc A.foo} */
+  foo: string;
+}
+
+export { }
---
+++ lib/index.d.ts
@@ -0,0 +1,17 @@
+/**
+ * @public
+ */
+export declare interface A {
+  /**
+   * This is a description of foo, which usually bars.
+   */
+  foo: string;
+}
+
+/**
+ * @public
+ */
+export declare interface B {
+  /** {@inheritDoc A.foo} */
+  foo: string;
+}
